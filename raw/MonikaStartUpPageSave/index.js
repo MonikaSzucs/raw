92be74d6c39646399ec7a63afc1f345557d7b2bf
Unknown <giano_patane@hotmail.com>
2017-10-03 19:38:00 -0700

    Tried to start Connecting the chat to Index.js
    i dont know what the fuck im doing, thats why its in a branch cheers

--- raw/MonikaStartUpPageSave/index.js
+++ raw/MonikaStartUpPageSave/index.js
@@ -3,6 +3,40 @@
 			var LogInButton = document.getElementById("LogInButton");
 			var wrapper = document.getElementById("wrapper");
 
+/////socket Chat Variables
+
+var users = { 
+username:"",
+msg:"",
+username2:""    
+    
+
+};
+
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    io.broadcast.emit(username, data);  
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+//////////
+
+
                 
             var Email = document.getElementById("Email");
             var Password = document.getElementById("Password");
