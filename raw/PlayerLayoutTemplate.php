4dda323bb943258045bf2137529b5da9133c0153
Unknown <monika.silvia.s@hotmail.ca>
2017-10-28 07:03:43 -0700

    fixed group styling and testing pages numbers

---
+++ raw/PlayerLayoutTemplate.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.group_container_create {
+			width: 800px; 
+			height: 150px; 
+			background-color:red;
+			
+		}
+		
+		.group_photo_Area {
+			width: 150px;
+			height: 150px;
+			float: left;
+		}
+		
+		.circlePhotoAuto {
+			width: 130px;
+			height: 130px;
+			margin-top: 10px;
+			margin-left: 10px;
+			-webkit-border-radius: 50%;
+			-moz-border-radius: 50%;
+			-ms-border-radius: 50%;
+			-o-border-radius: 50%;
+			border-radius: 50%; 
+		}
+		.vertical_space_group {
+			width: 630px;
+			height: 110px;
+			margin-top: 20px;
+			margin-right: 10px;
+			background-color:yellow;
+			float: right;
+		}
+		.groups_title_generate{
+			width: 100%;
+			height: 40px;
+			background-color:orange;
+		}
+		
+		.groups_descrition_generate{
+			width: 100%;
+			height: 50px;
+			background-color:purple;
+		}
+	</style>
+
+</head>
+<body>
+	<div class="group_container_create">
+		<div class="group_photo_Area">
+			<img src='./SVG/EmptyPicture.svg' class='circlePhotoAuto' />
+		</div>
+
+			<div class='vertical_space_group'>
+				<div class="groups_title_generate">
+					Title:
+				</div>
+			<hr/>
+				<div class="groups_descrition_generate">
+					Description
+				</div>
+			</div>
+	</div>
+		<!--
+		<div style='background-color:orange padding='></div>
+		<div style='background-color: blue'>
+			
+			<div class='ProfileIconGroups'>
+					<img src='./SVG/EmptyPicture.svg' class='circlePhotoAuto' />
+			</div>
+			<div class='vertical-space'></div>
+			<div class='GroupsInformation'>
+				<div class='GroupsInformation-Title'>test<br/></div>
+				<div class='horizontal-GroupSpace'></div>
+				<div class='GroupsInformation-Description'>description<br/></div>
+			</div>
+			
+		</div>
+		-->
+	
+
+</body>
+</html>
--- raw/imagetest.php
+++ raw/imagetest.php
@@ -1,25 +1,106 @@
 <?php
+	//the session_start() should always be at the top
+
+		/*
+		if(!isset($_SESSION["user_id"]))
+		{
+			session_destroy(); 
+			header( 'Location: signout.php' ); 
+		};
+		*/
+
+		//Step1 connect to database
+		define('DB_HOST', 'localhost');
+		define('DB_USER', 'root');
+		define('DB_PASSWORD', '');
+		define('DB_DATABASE', 'raw');
+
+		$db = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE) or die('Error connecting to MySQL server.');
+
+	$query  = "SELECT * FROM groups LIMIT ,0,10 # Retrieve rows 0-10";
+	
+$result = mysqli_query($db, $query) or die('Error querying database.');
+
+while ($row = mysqli_fetch_array($result)) 
+{
+	$row['group_title'];
+}
+
+
 	$a = array("1","2","3","4","5","6","7","8","9","10","11","12");
 	
 	$num = 0;
 	$display = 4;
 	$length = sizeof($a);
-	$page = 1;
+	$pages = ceil(sizeof($a)/5);
+	$show = 5;
+
+	echo $query;
+	
+/*
+	$k = 1;
 	echo "<pre>";
 	
-	function stuff(){
+	function back(){
 		
 	};
 	
 	
+	function forward(){
+		$show = 10;
+		echo "hi";
+	};
 	
-	while($num < $length){
+
+	
+	
+	$pageNum = $show;
+	$numPages = sizeof($a);
+	
+	$i = $pageNum - 5;
+	if ($i <= 0){	
+		$i = 1;
+		$j = $pageNum + 5;
+	}
+	if ($j > $numPages)	{
+		$j = $numPages;
+	}
+		
+	if ($i > 5)	{
+		echo '... ';
+	}
+	
+	for ($i; $i <= $j; $i++) { 		
+		echo " "; 		
+		if ($i == $pageNum) 			
+			echo "$i";		
+		else 			
+			echo "<a href=\"?page=$i\">$i</a>"; 
+	}
+	if($i < $numPages - 5){	
+		echo ' ...';
+	}
+	
+	
+
+	while($num < $show){
 		echo "<div style='background-color:red;width:50px;height:50px;margin-right:10px;float:left;'>$a[$num]</div>";
 		$num++;
-	}
+	};
 	echo "<br/><br/><br/><br/>";
-	echo "<div style='background-color:orange;width:50px;height:50px;margin-right:10px;float:left;' onclick='stuff()'>back</div>";
-	echo "<div style='background-color:yellow;width:50px;height:50px;margin-right:10px;float:left;'>Forward</div>";
+	echo "<div style='width: 500px; height: 55px; background-color: purple;'>";
+		echo "<div style='left: 0; right: 0; margin: 0 auto; text-align: center'>";
+			echo "<div style='height: 100%; background-color: pink; display: inline-block; position: relative'>";
+				echo "<div style='background-color:orange;width:50px;height:50px; float: left;' onclick='back()'>back</div>";
+				while ($k<=$pages){
+					echo "<div style='background-color:red; width: 20px; float:left;'>$k</div>";
+					$k++;
+				}
+				echo "<div style='background-color:yellow;width:50px;height:50px;margin-right:10px;float:left;' onclick='forward()'>Forward</div>";
+			echo "</div>";
+		echo "</div>";
+	echo "</div>";
 	
 	echo "</pre>";
+*/	
 ?>
