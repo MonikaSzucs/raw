0efec003cd12e49bf04800582c804d2547677c32
Unknown <monika.silvia.s@hotmail.ca>
2017-10-28 10:24:20 -0700

    added new database file

--- raw/databse file/raw.sql
+++ raw/databse file/raw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2017 at 06:30 AM
+-- Generation Time: Oct 28, 2017 at 07:22 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -41,35 +41,9 @@ CREATE TABLE `groups` (
 --
 
 INSERT INTO `groups` (`group_id`, `group_title`, `group_description`, `group_photo`, `created`) VALUES
-(6, 'xxxx444', 'ttttttttttttttt', '', '2017-10-26 20:43:56'),
-(11, '123', '132', '', '2017-10-26 20:43:56'),
-(14, '123xxxx', '132', '', '2017-10-26 20:43:56'),
-(15, 'Get', '132', '', '2017-10-26 20:43:56'),
-(16, 'Getwww', '132', '', '2017-10-26 20:43:56'),
-(17, 'Getwwwxxx', '132', '', '2017-10-26 20:43:56'),
-(18, 'Getwwwxxxxxx', '132', '', '2017-10-26 20:43:56'),
-(19, '456', '456', '', '2017-10-26 20:43:56'),
-(21, '4444434325', '4443254345', '', '2017-10-26 20:43:56'),
-(23, 'zzzzzzzzzzzz', 'sssss', '', '2017-10-26 20:43:56'),
-(24, 'qqqqq', 'zzzz', 'UserPictures/AAEAAQAAAAAAAAobAAAAJGZmYjJmYjgzLTZmOGUtNDY1NS04MzJlLTkyZjM5NDE3MThhYw.jpg', '2017-10-26 20:43:56'),
-(25, 'sfsdfds', 'sdfsdfdsf', 'UserPictures/0ca5025.jpg', '2017-10-26 20:43:56'),
-(26, 'dfgfd', 'fdgdfg', 'UserPictures/bird.png', '2017-10-26 20:43:56'),
-(27, 'Tessstt', 'bbbbbbbbbbbbbbbbbbbbbbb', 'UserPictures/dsfdsgdfgfdg.png', '2017-10-26 20:43:56'),
-(28, 'Tessstttttttttttttttttttttt', 'bbbbbbbbbbbbbbbbbbbbbbbffffffffffff', 'UserPictures/birddyyyyddd.png', '2017-10-26 20:43:56'),
-(29, 'yyyyyyyyyyyyyyyyyyyyyyy', 'yyyyrr', 'UserPictures/setC.png', '2017-10-26 20:43:56'),
-(30, 'yyyyyyyyyyyyyyyyyyyyyyy77', 'yyyyrr7777', 'UserPictures/q2.png', '2017-10-26 20:43:56'),
-(31, 'uuuuuuuuuuuuuuu', 'tttttttttttttttt', '', '2017-10-26 20:43:56'),
-(32, 'uuuuuuuuuuuuuuuccccc', 'ttttttttttttttttcccc', '', '2017-10-26 20:43:56'),
-(33, 'uuuuuuuuuuuuuuucccccdddsss', 'ttttttttttttttttccccdddss', '', '2017-10-26 20:43:56'),
-(34, '1233444342', '123324', 'UserPictures/q2.png', '2017-10-26 20:43:56'),
-(35, '1233444342bbbbbbbbb', '123324bbbbbbbbbbbbb', 'UserPictures/meeting.png', '2017-10-26 20:43:56'),
-(36, 'qqqqqq', 'qqqqqqqq', 'UserPictures/setC.png', '2017-10-26 20:43:56'),
-(37, 'SSSSSSSSSSSSSSSSSSS', 'SSAAAAAAAAAAAAAAAAAAAAA', 'UserPictures/1509075575download.jpg', '2017-10-26 20:43:56'),
-(38, '13213', '3123213', 'UserPictures/1509075683initial.png', '2017-10-26 20:43:56'),
-(39, 'new', 'new', 'UserPictures/1509075874meeting.png', '2017-10-26 20:44:34'),
-(40, 'aaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaa', 'UserPictures/1509077267dsfdsgdfgfdg.png', '2017-10-26 21:07:48'),
-(41, 'aaaaaaaaaaaaaaaddd', 'aaaaaaaaaaaaaaaaaaaaddd', '', '2017-10-26 21:09:30'),
-(42, 'aaaaaaaaaaaaaaadddttttttttttt', 'aaaaaaaaaaaaaaaaaaaadddtttttt', '', '2017-10-26 21:12:36');
+(44, 'The fox jumped over the fence', 'the brown fox jumped over the fence', 'UserPictures/1509162096setC.png', '2017-10-27 20:41:36'),
+(46, 'The brown fox jumped over the fence with a great l', 'The brown fox jumped over the fence with a great leap. The brown fox jumped over the fence with a great leap. The brown fox jumped over the fence with a great leap. The brown fox jumped over the fence with a great leap. The brown fox jumped over the fence with a great leap. The brown fox jumped over the fence with a great leap. The brown fox jumped over the fence with a great leap. ', '', '2017-10-27 20:43:33'),
+(48, 'testinggggg here is is a new one', 'test egsegdsg', 'UserPictures/1509206146dsfdsgdfgfdg.png', '2017-10-28 08:55:46');
 
 -- --------------------------------------------------------
 
@@ -87,30 +61,9 @@ CREATE TABLE `group_users` (
 --
 
 INSERT INTO `group_users` (`group_id`, `user_id`) VALUES
-(6, 1),
-(11, 1),
-(14, 1),
-(15, 1),
-(17, 1),
-(17, 9),
-(18, 1),
-(18, 10),
-(24, 1),
-(27, 1),
-(28, 1),
-(29, 1),
-(30, 1),
-(31, 1),
-(32, 1),
-(33, 1),
-(34, 1),
-(35, 1),
-(36, 1),
-(38, 1),
-(39, 1),
-(40, 1),
-(41, 1),
-(42, 1);
+(44, 1),
+(46, 1),
+(48, 1);
 
 -- --------------------------------------------------------
 
@@ -133,31 +86,30 @@ CREATE TABLE `music_group` (
   `g_funk` tinyint(1) NOT NULL,
   `g_country` tinyint(1) NOT NULL,
   `g_edm` tinyint(1) NOT NULL,
-  `g_classical` tinyint(1) NOT NULL
+  `g_classical` tinyint(1) NOT NULL,
+  `g_happy` tinyint(1) NOT NULL,
+  `g_sad` tinyint(1) NOT NULL,
+  `g_angry` tinyint(1) NOT NULL,
+  `g_chill` tinyint(1) NOT NULL,
+  `g_focus` tinyint(1) NOT NULL,
+  `g_workout` tinyint(1) NOT NULL,
+  `g_travel` tinyint(1) NOT NULL,
+  `g_guitar` tinyint(1) NOT NULL,
+  `g_bass` tinyint(1) NOT NULL,
+  `g_synth` tinyint(1) NOT NULL,
+  `g_pads` tinyint(1) NOT NULL,
+  `g_woodwind` tinyint(1) NOT NULL,
+  `g_drums` tinyint(1) NOT NULL,
+  `g_strings` tinyint(1) NOT NULL,
+  `g_brass` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `music_group`
 --
 
-INSERT INTO `music_group` (`group_id`, `music_id`, `music_file`, `music`, `music_uploaded`, `g_rock`, `g_rnb`, `g_pop`, `g_punk`, `g_jazz`, `g_metal`, `g_funk`, `g_country`, `g_edm`, `g_classical`) VALUES
-(26, 1, '', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(27, 2, 'UsersSongs/ChaCha_Fontanez.mp3', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(28, 3, '', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(29, 4, '', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(30, 5, 'UsersSongs/ChaCha_Fontanttez.mp3', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(31, 6, 'UsersSongs/Pachabel333ly.mp3', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(32, 7, 'UsersSongs/censor-beep-01.mp3', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(33, 8, 'UsersSongs/censor-besssep-01.mp3', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(34, 9, 'UsersSongs/censor-bess3333s22222ep-01.mp3', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(35, 10, 'UsersSongs/Pachabel333ly.mp3', NULL, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(36, 11, 'UsersSongs/censor-erebess3333s22222ep-01.mp3', 1, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(37, 12, 'UsersSongs/1509075575censor-erebess3333s22222ep-01.mp3', 1, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(38, 13, 'UsersSongs/1509075683censor-erebess3333s22222ep-01.mp3', 0, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(39, 14, 'UsersSongs/1509075874censor-erebess3333s22222ep-01.mp3', 0, '2017-10-26 20:46:50', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-(40, 15, 'UsersSongs/1509077267ChaCha_Fontanttez.mp3', 1, '2017-10-26 21:07:48', 1, 1, 0, 0, 0, 0, 0, 0, 1, 0),
-(41, 16, '', 1, '2017-10-26 21:09:30', 1, 1, 0, 0, 0, 0, 1, 1, 1, 0),
-(42, 17, '', 0, '2017-10-26 21:12:36', 0, 1, 0, 0, 0, 1, 1, 1, 1, 0);
+INSERT INTO `music_group` (`group_id`, `music_id`, `music_file`, `music`, `music_uploaded`, `g_rock`, `g_rnb`, `g_pop`, `g_punk`, `g_jazz`, `g_metal`, `g_funk`, `g_country`, `g_edm`, `g_classical`, `g_happy`, `g_sad`, `g_angry`, `g_chill`, `g_focus`, `g_workout`, `g_travel`, `g_guitar`, `g_bass`, `g_synth`, `g_pads`, `g_woodwind`, `g_drums`, `g_strings`, `g_brass`) VALUES
+(48, 23, 'UsersSongs/1509206146ChaCha_Fontanttez.mp3', 1, '2017-10-28 08:55:46', 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -234,12 +186,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `group_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 --
 -- AUTO_INCREMENT for table `music_group`
 --
 ALTER TABLE `music_group`
-  MODIFY `music_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `music_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `user`
 --
