cee091942cb185e2a2c7ad98df0c1c5a7dcbd6be
Unknown <monika.silvia.s@hotmail.ca>
2017-11-21 19:15:03 -0800

    got rid of unneeded files and trying to fix file path

--- raw/Streaming.php
+++ raw/Streaming.php
@@ -204,7 +204,7 @@ if(isset($_POST['toDo'])){
                         echo "</div>";
 						
 						///Make sure you change this directory area when we configure it for when we host it
-							echo "<a href='/rawNov18/raw/raw/". $row['music_file'] . "' download='" . $row['music_file'] . "'>";
+							echo "<a href='/raw/raw/raw/". $row['music_file'] . "' download='" . $row['music_file'] . "'>";
 								echo "<button class='download_feed_button'>Download</button>";
 								echo "</a>";
 						
--- raw/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$xxx = "MONIKA.mp3.pdf";
-echo "<br/>---------<br/>";
-$t=time();
-echo($t . "<br>");
-echo(date("Y-m-d",$t));
-	
-?>
-	
